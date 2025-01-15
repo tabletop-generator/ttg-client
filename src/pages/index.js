0ab8b29d43de1e9fbabdfcf3280472fb01207815
Chris Simon <Chris.m.simon1@gmail.com>
2025-01-15 17:00:40 -0500

    Initial setup and in-progress app shell

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import ApplicationShell from "../components/ApplicationShell";
+
+export default function Home() {
+  return (
+    <ApplicationShell>
+      <p>Holy balls I think it&apos;s working</p>
+    </ApplicationShell>
+  );
+}
