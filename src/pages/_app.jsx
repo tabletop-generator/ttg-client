be5d139fbb8c0a24b85a3fc5a6a4f1d27af139ea
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-13 01:20:50 -0400

    Add title and favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import Layout from "@/components/Layout.jsx";
 import withAuth from "@/components/auth/withAuth";
 import { UserProvider } from "@/context/UserContext";
 import "@/styles/tailwind.css";
+import Head from "next/head";
 import { AuthProvider } from "react-oidc-context";
 
 export default function App({ Component, pageProps }) {
@@ -12,12 +13,18 @@ export default function App({ Component, pageProps }) {
   const WrappedComponent = isProtectedPage ? withAuth(Component) : Component;
 
   return (
-    <AuthProvider {...cognitoAuthConfig}>
-      <UserProvider>
-        <Layout>
-          <WrappedComponent {...pageProps} />
-        </Layout>
-      </UserProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <title>Tabletop Generator</title>
+        <link rel="icon" href="/favicon.ico?v=2" />
+      </Head>
+      <AuthProvider {...cognitoAuthConfig}>
+        <UserProvider>
+          <Layout>
+            <WrappedComponent {...pageProps} />
+          </Layout>
+        </UserProvider>
+      </AuthProvider>
+    </>
   );
 }
